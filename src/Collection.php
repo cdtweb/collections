b4f995cfab6372fa0d5eff813f1496b0d2d3d5c4
Clint Tyler <ctyler@cdtwebsolutions.com>
2017-06-23 11:07:21 -0600

    Port over nonamephp/common Collection lib and tests

--- src/Collection.php
+++ src/Collection.php
@@ -1,8 +1,269 @@
 <?php declare(strict_types=1);
 
-namespace Cdtweb;
+namespace Cdtweb\Collection;
 
-class Collection implements CollectionInterface
+use ArrayAccess;
+use Countable;
+use IteratorAggregate;
+use Serializable;
+use JsonSerializable;
+
+/**
+ * Collection
+ *
+ * @package Cdtweb\Collection
+ */
+class Collection implements Countable, ArrayAccess, IteratorAggregate, Serializable, JsonSerializable
 {
+    /**
+     * Array of items.
+     *
+     * @var array $items
+     */
+    protected $items = [];
 
+    /**
+     * Collection constructer.
+     *
+     * @param array $items
+     */
+    public function __construct(array $items = [])
+    {
+        $this->items = $items;
+    }
+
+    ///////////////////////////////////
+    // Magic Methods
+
+    public function __set($key, $value)
+    {
+        $this->set($key, $value);
+    }
+
+    public function __get($key)
+    {
+        return $this->get($key);
+    }
+
+    public function __isset($key)
+    {
+        return $this->has($key);
+    }
+
+    public function __unset($key)
+    {
+        $this->delete($key);
+    }
+
+    ///////////////////////////////////
+    // Collection Methods
+
+    /**
+     * Make a collection from one or more arrays.
+     *
+     * @return Collection
+     */
+    public static function make(array ...$arrays): Collection
+    {
+        $items = [];
+        foreach ($arrays as $arr) {
+            $items += $arr;
+        }
+        return new self($items);
+    }
+
+    /**
+     * Add an item to the collection.
+     *
+     * @param string $key
+     * @param mixed $value
+     */
+    public function set($key, $value)
+    {
+        $this->items[$key] = $value;
+    }
+
+    /**
+     * Get an item from the collection. Returns $default if item cannot be found.
+     *
+     * Passing an array of item keys for the value of $key will result in multiple
+     * items being returned. Keys that are missing from the collection will be
+     * returned with a value of $default.
+     *
+     * @param mixed $key
+     * @param mixed $default
+     * @return mixed Will return $default if cannot find item
+     */
+    public function get($key, $default = null)
+    {
+        if (is_array($key)) {
+            // Get multiple items by their key
+            $items = [];
+            foreach ($key as $k) {
+                $items[$k] = isset($this->items[$k]) ? $this->items[$k] : $default;
+            }
+
+            return $items;
+        } else {
+            // Get a single item by its key
+            return isset($this->items[$key]) ? $this->items[$key] : $default;
+        }
+    }
+
+    /**
+     * Pluck an item from the collection. Returns $default if item cannot be found.
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    public function pluck($key, $default = null)
+    {
+        $value = $this->get($key, $default);
+
+        $this->delete($key);
+
+        return $value;
+    }
+
+    /**
+     * Gets all of the items in the collection as an array.
+     *
+     * @return array
+     */
+    public function all(): array
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Gets keys for all of the items in the collection.
+     *
+     * @return array
+     */
+    public function keys(): array
+    {
+        return array_keys($this->items);
+    }
+
+    /**
+     * Gets values for all of the items in the collection.
+     *
+     * @return array
+     */
+    public function values(): array
+    {
+        return array_values($this->items);
+    }
+
+    /**
+     * Checks for existence of an item in the collection.
+     *
+     * @param string $key
+     * @return boolean
+     */
+    public function has($key): bool
+    {
+        return isset($this->items[$key]);
+    }
+
+    /**
+     * Deletes an item from the collection.
+     *
+     * @param string $key
+     */
+    public function delete($key)
+    {
+        unset($this->items[$key]);
+    }
+
+    /**
+     * Delete all items in collection.
+     */
+    public function destroy()
+    {
+        $this->items = [];
+    }
+
+    /**
+     * Get collection as an array.
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * Get collection as JSON.
+     *
+     * @return string
+     */
+    public function toJson(): string
+    {
+        return json_encode($this);
+    }
+
+    ///////////////////////////////////
+    // IteratorAggregate Methods
+
+    public function getIterator()
+    {
+        foreach ($this->items as $key => $value) {
+            yield $key => $value;
+        }
+    }
+
+    ///////////////////////////////////
+    // Countable Methods
+
+    public function count()
+    {
+        return count($this->items);
+    }
+
+    ///////////////////////////////////
+    // ArrayAccess Methods
+
+    public function offsetExists($offset)
+    {
+        return $this->has($offset);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        return $this->set($offset, $value);
+    }
+
+    public function offsetUnset($offset)
+    {
+        return $this->delete($offset);
+    }
+
+    ///////////////////////////////////
+    // JsonSerializable Methods
+
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
+
+    ///////////////////////////////////
+    // Serializable Methods
+
+    public function serialize()
+    {
+        return serialize($this->items);
+    }
+
+    public function unserialize($serialized)
+    {
+        $this->items = unserialize($serialized);
+    }
 }
--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -1,10 +1,214 @@
 <?php declare(strict_types=1);
 
-namespace Cdtweb;
+namespace Cdtweb\Collection;
 
 use PHPUnit\Framework\TestCase;
 
 class CollectionTest extends TestCase
 {
+    protected $collection;
 
+    protected $basicCollection = [
+        'key1' => 'val1',
+        'key2' => 'val2',
+        'key3' => 'val3',
+        'key4' => 'val4',
+    ];
+
+    protected function setUp()
+    {
+        $this->collection = new Collection;
+    }
+
+    ///////////////////////////////////
+    // Data providers
+
+    public function collectionItemsProvider()
+    {
+        return [
+            ['key1', 'val1'],
+            ['key2', 'val2'],
+            ['key3', 'val3'],
+            ['key4', 'val4'],
+        ];
+    }
+
+    ///////////////////////////////////
+    // Tests
+
+    /**
+     * @covers Collection::make
+     */
+    public function testMake()
+    {
+        $items_1 = ['key1' => 'val1'];
+        $items_2 = ['key2' => 'val2'];
+        $items_3 = ['key3' => 'val3'];
+        $collection = Collection::make($items_1, $items_2, $items_3);
+        $this->assertTrue($collection->has('key1'));
+        $this->assertTrue($collection->has('key2'));
+        $this->assertTrue($collection->has('key3'));
+    }
+
+    /**
+     * @covers Collection::count
+     */
+    public function testEmpty()
+    {
+        $this->assertTrue($this->collection->count() == 0);
+    }
+
+    /**
+     * @covers       Collection::set, Collection:get
+     * @dataProvider collectionItemsProvider
+     */
+    public function testSet($key, $value)
+    {
+        $this->collection->set($key, $value);
+        $this->assertEquals($value, $this->collection->get($key));
+    }
+
+    /**
+     * @covers       Collection::has
+     * @dataProvider collectionItemsProvider
+     */
+    public function testHas($key, $value)
+    {
+        $this->collection->set($key, $value);
+        $this->assertTrue($this->collection->has($key));
+    }
+
+    /**
+     * @covers       Collection::pluck
+     * @dataProvider collectionItemsProvider
+     */
+    public function testPluck($key, $value)
+    {
+        $this->collection->set($key, $value);
+        $plucked = $this->collection->pluck($key);
+        $this->assertEquals($plucked, $value);
+        $this->assertFalse($this->collection->has($key));
+    }
+
+    /**
+     * @covers       Collection::all, Collection::toArray, Collection::keys, Collection::values
+     * @dataProvider collectionItemsProvider
+     */
+    public function testToArray($key, $value)
+    {
+        $this->collection->set($key, $value);
+        $arr = $this->collection->toArray();
+        $keys = $this->collection->keys();
+        $values = $this->collection->values();
+
+        $this->assertTrue(is_array($arr));
+        $this->assertEquals($arr[$key], $value);
+        $this->assertEquals($keys, array_keys($arr));
+        $this->assertEquals($values, array_values($arr));
+    }
+
+    /**
+     * @covers       Collection::toJson
+     * @dataProvider collectionItemsProvider
+     */
+    public function testToJson($key, $value)
+    {
+        $this->collection->set($key, $value);
+        $json = json_encode($this->collection);
+
+        $this->assertEquals($json, $this->collection->toJson());
+    }
+
+    /**
+     * @covers       Collection::delete, Collection::count
+     * @dataProvider collectionItemsProvider
+     */
+    public function testDelete($key, $value)
+    {
+        $this->collection->set($key, $value);
+        $this->assertTrue($this->collection->count() == 1);
+        $this->collection->delete($key);
+        $this->assertFalse($this->collection->get($key, false));
+    }
+
+    /**
+     * @covers       Collection::destroy, Collection::count
+     * @dataProvider collectionItemsProvider
+     */
+    public function testDestroy($key, $value)
+    {
+        $this->collection->set($key, $value);
+        $this->assertTrue($this->collection->count() == 1);
+        $this->collection->destroy();
+        $this->assertTrue($this->collection->count() == 0);
+    }
+
+    /**
+     * @covers       Collection::getIterator
+     * @dataProvider collectionItemsProvider
+     */
+    public function testGetIterator($key, $value)
+    {
+        $this->collection->set($key, $value);
+        $this->assertEquals(iterator_to_array($this->collection->getIterator()), $this->collection->toArray());
+    }
+
+    /**
+     * @covers       Collection::jsonSerialize
+     * @dataProvider collectionItemsProvider
+     */
+    public function testJsonEncode($key, $value)
+    {
+        $this->collection->set($key, $value);
+        $json = json_encode($this->collection);
+        $this->assertEquals(json_decode($json, true), $this->collection->toArray());
+    }
+
+    /**
+     * @covers Collection::serialize, Collection::unserialize
+     */
+    public function testSerialize()
+    {
+        $data = [
+            'key1' => 'value1',
+            'key2' => 'value2'
+        ];
+
+        $collection1 = new Collection($data);
+        $serializedCollection = serialize($collection1);
+        $collection2 = unserialize($serializedCollection);
+        $this->assertEquals($collection1, $collection2);
+    }
+
+    /**
+     * @covers Collection::__set, Collection::__get, Collection::__isset, Collection::__unset
+     */
+    public function testMagicMethods()
+    {
+        $collection = new Collection;
+
+        $collection['key1'] = 'value1';
+
+        $this->assertTrue($collection->has('key1'));
+        $this->assertEquals($collection->get('key1'), 'value1');
+        $this->assertEquals($collection['key1'], 'value1');
+
+        unset($collection['key1']);
+        $this->assertFalse($collection->has('key1'));
+    }
+
+    /**
+     * @covers Collection::get
+     */
+    public function testGetMultipleItems()
+    {
+        $collection = new Collection(['key1' => 'value1', 'key2' => 'value2']);
+        $values = $collection->get(['key1', 'key2', 'key3']);
+
+        $this->assertTrue(array_key_exists('key1', $values) && $values['key1'] == 'value1');
+        $this->assertTrue(array_key_exists('key2', $values) && $values['key2'] == 'value2');
+
+        // Assert the key3 equals the default (null)
+        $this->assertTrue(array_key_exists('key3', $values) && $values['key3'] == null);
+    }
 }
